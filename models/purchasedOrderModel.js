3ee9c7e1a93fa557d15f00823d1d6f134999c832
DakshCodes <honeysingh617522@gmail.com>
2023-07-25 12:57:49 +0530

    Models Schema by Daksh

---
+++ models/purchasedOrderModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+
+const PurchasedSchema = mongoose.Schema({
+    order_date: {
+        type: String,
+        required: true,
+    },
+    supplier_name: {
+        type: String,
+        required: true,
+    },
+    refrence_no: {
+        type: String,
+        required: true,
+    },
+    deleveryat: {
+        type: String,
+        required: true,
+    },
+    terms_condition: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true,
+    },
+
+}, {
+
+});
+
+const purchaseorder = mongoose.model("finishproducts", PurchasedSchema);
+module.exports = purchaseorder;
