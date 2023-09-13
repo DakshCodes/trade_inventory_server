c35fdb2956db6fccf1eb19fde0a0ec24dc4cd35e
DakshCodes <honeysingh617522@gmail.com>
2023-09-13 13:42:33 +0530

    Add Firm Model Update2

--- models/firmModel.js
+++ models/firmModel.js
@@ -1,84 +1,80 @@
 const mongoose = require("mongoose")
 
 const firmSchema = new mongoose.Schema({
-    firm_name : {
-        type : String,
-        required : true,
-        default : "",
+    firm_name: {
+        type: String,
+        required: true,
+        default: "",
     },
-    firm_code : {
-        type : String,
-        required : true,
-        default : "",
+    firm_code: {
+        type: String,
+        required: true,
+        default: "",
     },
-    firm_mobile_no : {
-        type : String,
-        required : true,
-        default : "",
-    },
-
-    userId : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "users",
-        required : true,
+    firm_mobile_no: {
+        type: String,
+        required: true,
+        default: "",
     },
 
-    firm_address : {
-        type : String,
-        required : true,
-        default : "",
-    },
-    firm_bank_AC_name : {
-        type : String,
-        required : true,
-        default : "",
-    },
-    firm_bank_address : {
-        type : String,
-        required : true,
-        default : "",
-    },
-    contact_person : {
-        type : String,
-        required : true,
-        default : "",
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true,
     },
 
-    person_email : {
-        type : String,
-        required : true,
-        default : "",
+    firm_address: {
+        type: String,
+        required: true,
+        default: "",
     },
-    person_bank_AC_no : {
-        type : String,
-        required : true,
-        default : "",
+    firm_bank_AC_name: {
+        type: String,
+        required: true,
+        default: "",
     },
-    person_bank_name : {
-        type : String,
-        required : true,
-        default : "",
+    firm_bank_address: {
+        type: String,
+        required: true,
+        default: "",
     },
-    person_bank_IFSC : {
-        type : String,
-        required : true,
-        default : "",
+    contact_person: {
+        type: String,
+        required: true,
+        default: "",
     },
-    footer_img : {
-        type : Array,
-        default : [],
-        required : true,  
-    },
-    header_img : {
-        type : Array,
-        default : [],
-        required : true,  
-    },
-    
 
-},{
-    timestamps : true
+    person_email: {
+        type: String,
+        required: true,
+        default: "",
+    },
+    person_bank_AC_no: {
+        type: String,
+        required: true,
+        default: "",
+    },
+    person_bank_name: {
+        type: String,
+        required: true,
+        default: "",
+    },
+    person_bank_IFSC: {
+        type: String,
+        required: true,
+        default: "",
+    },
+    footer_img: {
+        type: String,
+        required: true,
+    },
+    header_img: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true
 })
 
-const firms = mongoose.model("firms",firmSchema);
+const firms = mongoose.model("firms", firmSchema);
 module.exports = firms;
